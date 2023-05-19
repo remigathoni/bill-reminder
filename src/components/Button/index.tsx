41ab2528a477add53d2fb64b08ddefbae0ec2d77
Mary Gathoni <maryg.kahiga@gmail.com>
2023-05-19 14:24:14 +0300

    style: Add animation to input and button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import { motion } from "framer-motion";
 
 type ButtonProps = {
   children?: React.ReactNode;
@@ -11,13 +12,16 @@ type ButtonProps = {
 
  const Button = ({children, backGroundColor, text, handler}:ButtonProps) => {
   return (
-    <button className={` border border-black p-2 rounded flex items-center justify-center w-full ${backGroundColor}`} onClick={handler}>
+    <motion.button 
+    whileHover={{ scale: 1.01, backgroundColor:"#f1f2eb" }}
+    whileTap={{ scale: 0.99 }}
+    className={` border border-black p-2 rounded flex items-center justify-center w-full ${backGroundColor}`} onClick={handler}>
         <div className="flex items-center">
             {children}
             <div className="text-md ml-2">{text}</div>
         </div>
         
-    </button>
+    </motion.button>
   )
 }
 export default Button
--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 
 
 interface inputProps {
@@ -9,7 +10,9 @@ const Input = ({label, handler, value}: inputProps) => {
     return(
         <div>
             <label htmlFor={label} className="text-gray-500 text-sm">{label}</label>
-            <input type="text" id={label} value={value} onChange={handler} className="border border-gray-500 p-2 rounded flex items-center justify-center w-full"/>
+            <motion.input 
+            whileFocus={{ boxShadow: "2px 2px 6px 2px #E8B650"}}
+            type="text" id={label} value={value} onChange={handler} className="border border-gray-500 p-2 rounded flex items-center justify-center w-full focus:outline-0"/>
         </div>
     )
 }
