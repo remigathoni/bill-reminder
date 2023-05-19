0fc9a840be550a8c7bbb23ec8978d89d52999ecb
Mary Gathoni <maryg.kahiga@gmail.com>
2023-05-19 13:44:17 +0300

    chore: Rename Button component

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-import Button from "@/components/Button/ButtonIcon.jsx";
+import Button from "@/components/Button";
 import Input from "@/components/Input";
 import Logo from "@/components/Logo";
 import React, { useState } from "react";
