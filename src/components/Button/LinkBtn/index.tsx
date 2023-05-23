db6256c7186e2156ba400a2be992dd6a87207458
Mary Gathoni <maryg.kahiga@gmail.com>
2023-05-23 13:45:39 +0300

    chore: Add bg color prop

--- src/components/Button/LinkBtn/index.tsx
+++ src/components/Button/LinkBtn/index.tsx
@@ -4,15 +4,16 @@ import Link from "next/link";
 
 interface LinkBtn {
     href: string,
-    text: string
+    text: string,
+    bgColor?: string
 }
-const LinkBtn = ({href, text}: LinkBtn) => {
+const LinkBtn = ({href, text, bgColor}: LinkBtn) => {
     return (
-        <Link href={href} className="font-bold">
+        <Link href={href} className={` font-semibold`}>
             <motion.button 
                 whileHover={{ scale: 0.98, backgroundColor:"#f1f2eb" }}
                 whileTap={{ scale: 0.99 }}
-                className="p-2 rounded-lg border-2 border-black">
+            className={`p-2 rounded-lg border-2 border-black ${bgColor}`}>
                 {text}
             </motion.button>
         </Link>
